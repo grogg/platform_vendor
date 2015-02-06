3bd7e648a5fea43e60dd49d10db7c5e33e340c19
Joshua Lee <lee.joshwa@gmail.com>
2015-02-05 18:30:19 -0700

    Add SuperSU (1/2) - 2 of 2 is located in platform_build - credits to AICP

--- extras/device-partial.mk
+++ extras/device-partial.mk
@@ -1,6 +1,8 @@
 # Extra Packages and Files
 PRODUCT_COPY_FILES += \
     vendor/extras/etc/init.d/99fstrim:system/etc/init.d/99fstrim:extras \
+    vendor/extras/supersu/SuperSU.zip:system/supersu/SuperSU.zip \
+    vendor/extras/supersu/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
 
 PRODUCT_PACKAGES += \
     AdAway
