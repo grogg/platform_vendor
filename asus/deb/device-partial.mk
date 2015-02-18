a1b20bf48235174772d97b45f1b86ec7d1ee6431
Joshua Lee <lee.joshwa@gmail.com>
2015-02-17 18:58:21 -0700

    Drop factory bootanimation

--- asus/deb/device-partial.mk
+++ asus/deb/device-partial.mk
@@ -20,5 +20,3 @@ PRODUCT_COPY_FILES := \
     vendor/asus/deb/proprietary/libjni_latinime.so:system/lib/libjni_latinime.so:asus \
     vendor/asus/deb/proprietary/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so:asus \
     vendor/asus/deb/proprietary/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so:asus \
-    vendor/extras/media/bootanimation_1200.zip:system/media/bootanimation.zip:extras \
-
