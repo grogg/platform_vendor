4656e385dcbed8710adde42d28019f8ecd10ebe8
Joshua Lee <lee.joshwa@gmail.com>
2015-03-26 19:56:07 -0700

    hammerhead: update to 5.1.0_r3

--- qcom/hammerhead/device-partial.mk
+++ qcom/hammerhead/device-partial.mk
@@ -170,8 +170,6 @@ PRODUCT_COPY_FILES := \
     vendor/qcom/hammerhead/proprietary/libsensor_user_cal.so:system/vendor/lib/libsensor_user_cal.so:qcom \
     vendor/qcom/hammerhead/proprietary/libtime_genoff.so:system/vendor/lib/libtime_genoff.so:qcom \
     vendor/qcom/hammerhead/proprietary/libTimeService.so:system/vendor/lib/libTimeService.so:qcom \
-    vendor/qcom/hammerhead/proprietary/libthermalclient.so:system/vendor/lib/libthermalclient.so:qcom \
-    vendor/qcom/hammerhead/proprietary/libthermalioctl.so:system/vendor/lib/libthermalioctl.so:qcom \
     vendor/qcom/hammerhead/proprietary/librpmb.so:system/vendor/lib/librpmb.so:qcom \
     vendor/qcom/hammerhead/proprietary/libssd.so:system/vendor/lib/libssd.so:qcom \
     vendor/qcom/hammerhead/proprietary/qmi_fw.conf:system/vendor/lib/qmi_fw.conf \
