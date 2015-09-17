57f82eb95f87c4504f095d467e84a690f50532ca
Joshua Lee <lee.joshwa@gmail.com>
2015-09-16 21:46:33 -0500

    extras: add marshmallow bootanimation.zip

--- extras/device-partial.mk
+++ extras/device-partial.mk
@@ -1,8 +1,9 @@
 # Extra Packages and Files
 PRODUCT_COPY_FILES += \
     vendor/extras/etc/init.d/99fstrim:system/etc/init.d/99fstrim:extras \
-    vendor/extras/supersu/SuperSU.zip:system/supersu/SuperSU.zip \
-    vendor/extras/supersu/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon \
+    vendor/extras/media/bootanimation.zip:system/media/bootanimation.zip:extras \
+    vendor/extras/supersu/SuperSU.zip:system/supersu/SuperSU.zip:extras \
+    vendor/extras/supersu/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon:extras \
 
 PRODUCT_PACKAGES += \
     AdAway \
